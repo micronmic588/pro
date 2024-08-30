a68b08aa8ec9dddcc056e75babb7bcb93a31f2e5
Micron588 <95529730+micronmic588@users.noreply.github.com>
2024-08-30 18:05:29 +0600

    Update draw.js

--- draw.js
+++ draw.js
@@ -7,7 +7,7 @@ module.exports.config = {
     usage: "/draw <prompt>",
     role: "user",
     usePrefix: true,
-    aliases: ["image"],
+    aliases: ["image","gitstalk"],
     author: "MICRON",
 };
 
