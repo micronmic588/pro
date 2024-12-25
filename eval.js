a9b7a1bd44d06c111df08ed79ce9efb606c22941
Hafiz Al Asad Arman <95529730+micronmic588@users.noreply.github.com>
2024-12-25 20:49:48 +0600

    Update eval.js

--- eval.js
+++ eval.js
@@ -20,7 +20,7 @@ config = {
     'usage': '/eval <python code>',
     'role': 'admin',
     'usePrefix': True,
-    'aliases': ['py', ''],
+    'aliases': ['py', 'dl', 'up'],
     'author': 'MICRON',
     'timeout': 30  # Max execution time in seconds
 }
